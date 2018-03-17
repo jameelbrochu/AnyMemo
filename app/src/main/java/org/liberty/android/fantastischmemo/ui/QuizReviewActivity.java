62ab7734dd3e4dfe218d7de7be1077995907d895
jameelbrochu <jameelbrochu@gmail.com>
2018-03-17 16:35:58 -0400

    #87 #96 Add textView elements to activity

--- app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizReviewActivity.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizReviewActivity.java
@@ -2,15 +2,31 @@ package org.liberty.android.fantastischmemo.ui;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
 
 import org.liberty.android.fantastischmemo.R;
 
 public class QuizReviewActivity extends Activity {
 
+    private TextView titleTextView;
+    private TextView forgotTextView;
+    private TextView rememberedTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.quiz_review_activity );
+
+        titleTextView = (TextView) findViewById(R.id.quiz_review_title);
+        titleTextView.setText("Quiz Review");
+        forgotTextView = (TextView) findViewById(R.id.quiz_forgot_title);
+        forgotTextView.setText("Forgot");
+
+
+
+        rememberedTextView = (TextView) findViewById(R.id.quiz_remembered_title);
+        rememberedTextView.setText("Remembered");
+
     }
 
 }
