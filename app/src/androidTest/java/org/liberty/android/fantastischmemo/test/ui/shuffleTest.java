ebb81d3ecd20561774742c43ea382a563143e57f
rekandiah <reginakandiah.cv@gmail.com>
2018-03-19 00:59:18 -0400

    #19 Espresso Shuffle Test

---
+++ app/src/androidTest/java/org/liberty/android/fantastischmemo/test/ui/shuffleTest.java
@@ -0,0 +1,102 @@
+package org.liberty.android.fantastischmemo.test.ui;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.liberty.android.fantastischmemo.R;
+import org.liberty.android.fantastischmemo.ui.AnyMemo;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isFocusable;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class shuffleTest {
+
+    @Rule
+    public ActivityTestRule<AnyMemo> mActivityTestRule = new ActivityTestRule<>(AnyMemo.class);
+
+    @Test
+    public void shuffleTest() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.test_shuffle),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.FrameLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.card_text_view), withText("?\nShow answer"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.root),
+                                        0),
+                                0)));
+        appCompatTextView.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.grade_button_5), withText("Easy\n5.0 day"),
+                        childAtPosition(
+                                allOf(withId(R.id.grade_buttons_anki),
+                                        childAtPosition(
+                                                withId(R.id.buttons_root),
+                                                0)),
+                                5),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.small_title_bar), withText("Today: 1 New: 27 Rev: 0 ID: 13 C: French (Body parts)"),
+                        childAtPosition(
+                                allOf(withId(R.id.root),
+                                        childAtPosition(
+                                                withId(R.id.gesture_overlay),
+                                                0)),
+                                0),
+                        isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
