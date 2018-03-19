a9bf7c17eda3244b7ff6355511fda148393a98db
jameelbrochu <jameelbrochu@gmail.com>
2018-03-18 22:37:44 -0400

    #87 Cleanup code in QuizReviewActivity

--- app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizReviewActivity.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizReviewActivity.java
@@ -7,9 +7,7 @@ import android.widget.TextView;
 import org.liberty.android.fantastischmemo.R;
 import org.liberty.android.fantastischmemo.entity.Card;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 public class QuizReviewActivity extends Activity {
 
@@ -29,10 +27,9 @@ public class QuizReviewActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.quiz_review_activity );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_review_activity);
         try {
-            //Bundle extras = getIntent().getExtras();
             all_forgotten_cards = this.getIntent().getParcelableArrayListExtra(FORGOT_CARDS);
             all_remembered_cards = this.getIntent().getParcelableArrayListExtra(REMEMBERED_CARDS);
         }
@@ -46,22 +43,7 @@ public class QuizReviewActivity extends Activity {
 
         forgotQuestionsTextView = (TextView)findViewById(R.id.quiz_forgot_questions);
 
-/*
-        ArrayList<Card> forgotCards = new ArrayList<>();
-        Card fcard1 = new Card();
-        fcard1.setQuestion("Question1");
-        fcard1.setId(1);
-        fcard1.setAnswer("Answer1");
-        forgotCards.add (fcard1);
-
-        Card fcard2 = new Card();
-        fcard2.setQuestion("Question2");
-        fcard2.setId(2);
-        fcard2.setAnswer("Answer2");
-        forgotCards.add (fcard2);
-*/
-
-        for(int i=0; i < all_forgotten_cards.size(); i++){
+        for(int i=0; i < all_forgotten_cards.size(); i++) {
             Card c = all_forgotten_cards.get(i);
             forgotQuestionsTextView.setText(forgotQuestionsTextView.getText() + "Question " + Integer.toString(c.getId()) + ": " + c.getQuestion() + "\nAnswer: " + c.getAnswer()
                     + "\n\n");
@@ -72,7 +54,7 @@ public class QuizReviewActivity extends Activity {
 
         rememberQuestionsTextView = (TextView)findViewById(R.id.quiz_remember_questions);
 
-        for(int i=0; i < all_remembered_cards.size(); i++){
+        for(int i=0; i < all_remembered_cards.size(); i++) {
             Card c = all_remembered_cards.get(i);
             rememberQuestionsTextView.setText(rememberQuestionsTextView.getText() + "Question " + Integer.toString(c.getId()) + ": " + c.getQuestion() + "\nAnswer: " + c.getAnswer()
                     + "\n\n");
