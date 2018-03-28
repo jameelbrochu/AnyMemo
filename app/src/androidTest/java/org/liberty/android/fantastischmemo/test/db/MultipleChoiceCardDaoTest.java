b79dec1542e018f56398860ec40a3a0f3944696f
rekandiah <reginakandiah.cv@gmail.com>
2018-03-28 17:19:51 -0400

    #104 #116 fixes to tests because of corey

--- app/src/androidTest/java/org/liberty/android/fantastischmemo/test/db/MultipleChoiceCardDaoTest.java
+++ app/src/androidTest/java/org/liberty/android/fantastischmemo/test/db/MultipleChoiceCardDaoTest.java
@@ -79,6 +79,12 @@ public class MultipleChoiceCardDaoTest extends AbstractExistingDBTest {
     public void testGetMultipleChoiceCardById() throws Exception {
         MultipleChoiceCard mc = multipleChoiceCardDao.getMultipleChoiceCard(1);
         assertEquals(mc.getAnswer(), "blue");
+        assertEquals(mc.getQuestion(), "What color is the sky?");
+        assertEquals(mc.getOption1(), "green");
+        assertEquals(mc.getOption2(), "blue");
+        assertEquals(mc.getOption3(), "pink");
+        assertEquals(mc.getOption4(), "orange");
+
     }
 
     @SmallTest
@@ -90,11 +96,18 @@ public class MultipleChoiceCardDaoTest extends AbstractExistingDBTest {
         assertNotNull(multipleChoiceCardDao.getMultipleChoiceCard(4).getAnswer());
         assertEquals(multipleChoiceCardDao.getAllMultipleChoiceCards().size(), 4);
         assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(4).getAnswer(), newCard.getAnswer());
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(4).getQuestion(), newCard.getQuestion());
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(4).getOption1(), newCard.getOption1());
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(4).getOption2(), newCard.getOption2());
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(4).getOption3(), newCard.getOption3());
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(4).getOption4(), newCard.getOption4());
+
+
     }
 
     @SmallTest
     @Test
-    public void testDeleteLastMultipleChoiceCard() {
+    public void testDeleteLastMultipleChoiceCardRow() {
         MultipleChoiceCard cardToDelete = multipleChoiceCardDao.getMultipleChoiceCard(3);
         assertNotNull(cardToDelete);
         assertEquals(multipleChoiceCardDao.getAllMultipleChoiceCards().size(), 3);
@@ -111,11 +124,24 @@ public class MultipleChoiceCardDaoTest extends AbstractExistingDBTest {
         assertNotNull(cardToDelete);
         assertEquals(multipleChoiceCardDao.getAllMultipleChoiceCards().size(), 3);
         assertEquals(cardToDelete.getAnswer(), "green");
+        assertEquals(cardToDelete.getQuestion(), "What color is grass?");
+        assertEquals(cardToDelete.getOption1(), "green");
+        assertEquals(cardToDelete.getOption2(), "blue");
+        assertEquals(cardToDelete.getOption3(), "pink");
+        assertEquals(cardToDelete.getOption4(), "orange");
+
         multipleChoiceCardDao.deleteMultipleChoiceCard(cardToDelete);
         List<MultipleChoiceCard> updatedList = multipleChoiceCardDao.getAllMultipleChoiceCards();
         assertEquals(updatedList.size(), 2);
         assertNotNull(multipleChoiceCardDao.getMultipleChoiceCard(2));
         assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(2).getAnswer(), "white");
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(2).getQuestion(), "What color is snow?");
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(2).getOption1(), "green");
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(2).getOption2(), "blue");
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(2).getOption3(), "white");
+        assertEquals(multipleChoiceCardDao.getMultipleChoiceCard(2).getOption4(), "orange");
+
+
 
     }
 
