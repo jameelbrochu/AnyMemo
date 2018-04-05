e567ce0edbd93a127bdb846e416c11f58de085e2
Corey Levine <coreylevine123@gmail.com>
2018-04-05 15:34:43 -0400

    #82 #145 #146 Created QuizHistoryActivity and xml

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/QuizHistoryActivity.java
@@ -0,0 +1,15 @@
+package org.liberty.android.fantastischmemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.liberty.android.fantastischmemo.R;
+
+public class QuizHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_history_activity);
+    }
+}
